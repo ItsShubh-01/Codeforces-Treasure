d6ed25adc9b72671d52d0f9fa659665788260c54
ItsShubh-01 <shubhagarwalaaaaa@gmail.com>
2026-05-03 16:06:37 +0000

    Codeforces 1866A: Ambitious Kid

---
+++ 1866A-ambitious-kid/solution.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n; cin >> n;
+    int mini = INT_MAX;
+    for(int i=0; i<n; i++)
+    {
+        int num; cin >> num;
+        mini = min(mini,abs(num));
+    }
+    cout << mini << endl;
+    return 0;
+}
+
+// Not submitted
