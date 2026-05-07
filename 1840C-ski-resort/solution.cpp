99b651f9ee28fa99cf9ffec0d2937736fc868b6c
ItsShubh-01 <shubhagarwalaaaaa@gmail.com>
2026-05-07 19:47:30 +0000

    Codeforces 1840C: Ski Resort

---
+++ 1840C-ski-resort/solution.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define vstr vector<string>
+#define vb vector<bool>
+#define vvb vector<vector<bool>>
+#define vpii vector<pair<int,int>>
+#define pll pair<ll, ll>
+#define vpl vector<pll>
+#define umap unordered_map<int,int>
+#define umapll unordered_map<int,ll>
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
+#define forr(i, a, b) for (int i = a; i <= b; i++)
+#define pb push_back
+#define ppb pop_back
+#define ins insert
+#define ers erase
+#define srt(v) sort(all(v))
+#define rsrt(v) sort(rall(v))
+#define rev(v) reverse(all(v))
+#define sum(v) accumulate(all(v), 0LL)
+#define mx(v) *max_element(all(v))
+#define mn(v) *min_element(all(v))
+#define cnt(v, x) count(all(v), (x))
+#define yes cout << "Yes" << '\n'
+#define no cout << "No" << '\n'
+
+const int SINF = 1000000000;
+const ll INF = 1e18;
+const int MOD = 1e9 + 7;
+const int MAXN = 4e5 + 5;
+const int N = 1e5 + 1;
+const int LOG = 20;
+
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+void solve() {
+	int n, k ,q;
+    cin >> n >> k >> q;
+    vi arr(n);
+    for(int& i:arr) cin >> i;
+
+    ll count = 0, sum = 0;
+    rep(i,0,n){
+        if(arr[i]>q){
+            if(count>=k)
+                sum+=((count-k+1)*(count-k+2)/2);
+            count=0;
+        }
+        else count++;
+    }
+    if(count>=k) sum+=((count-k+1)*(count-k+2)/2);
+    cout << sum << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    
+	return 0;
+}
