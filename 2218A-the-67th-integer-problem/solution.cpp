e0c3acb3745b5e41767e96d49772415cba3d39ff
ItsShubh-01 <shubhagarwalaaaaa@gmail.com>
2026-06-03 18:25:12 +0000

    Codeforces 2218A: The 67th Integer Problem

---
+++ 2218A-the-67th-integer-problem/solution.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        if(n==67) cout << n << '\n';
+        else cout << n+1 << '\n';
+    }
+    return 0;
+}
