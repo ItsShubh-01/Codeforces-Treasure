d513707d9b9ffad309ae243d8b88600d5db96c0a
ItsShubh-01 <shubhagarwalaaaaa@gmail.com>
2026-08-25 15:44:00 +0000

    Codeforces 1703A: YES or YES?

---
+++ 1703A-yes-or-yes/solution.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while(t--) {
+        string s; cin >> s;
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+        if(s=="yes" || s=="no") cout << "YES" << '\n';
+        else cout << "NO" << '\n';
+    }
+    return 0;
+}
