b0c3d4f2a45768bae70c964a03e75a321a8e6509
ItsShubh-01 <shubhagarwalaaaaa@gmail.com>
2026-09-01 16:19:22 +0000

    Codeforces 2171A: Shizuku Hoshikawa and Farm Legs

---
+++ 2171A-shizuku-hoshikawa-and-farm-legs/solution.cpp
@@ -0,0 +1,11 @@
+    #include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        if(n%2) cout<<0<<'\n';
+        else cout << 1+n/4 << '\n';
+    }
+}
